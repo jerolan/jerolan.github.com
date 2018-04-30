94079c8ad825d84086149aa9248e5ec5d689b12a
Jerome Olvera <jerome.olvera@gmail.com>
2018-04-30 00:35:09 -0500

    fixed missing comma

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ const Layout = ({ children, data }) => (
     <Helmet
       title='『夢工場ドキドキパニック』'
       meta={[
-        { name: 'og:image', content: 'https://cdn.shopify.com/s/files/1/1065/1408/products/solid_black_backdrop_b52330dc-6531-42e2-bff3-28f7d6dff06c_large.png?v=1484054004' }
+        { name: 'og:image', content: 'https://cdn.shopify.com/s/files/1/1065/1408/products/solid_black_backdrop_b52330dc-6531-42e2-bff3-28f7d6dff06c_large.png?v=1484054004' },
         { name: 'description', content: 'Personal frontpage' }
       ]}
     />
