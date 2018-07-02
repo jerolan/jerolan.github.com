727f1d4d6aa5efd0286c98c4fd668dfe39ca0baa
Jerome Olvera <jerome.olvera@gmail.com>
2018-07-02 12:19:06 -0500

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,20 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-jsx'
+    'gatsby-plugin-styled-jsx',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-64555470-7",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ]
 }
