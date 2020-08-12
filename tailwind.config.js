0b612832be45ac8f922b679c1082c75901aa0cc6
jerolan <jerome.olvera@gmail.com>
2020-08-12 17:18:44 -0500

    update site content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: ["**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#282C35",
+        blue: "#91C5D3",
+        red: "#F1A5AB",
+        white: "#fff",
+        yellow: "#FFE3A9",
+      },
+    },
   },
   variants: {},
   plugins: [],
