7b0c9e9b6af022763b9e1a8ede35af9eb7fa23f6
Jerome Olvera <jerome.olvera@gmail.com>
2021-07-12 23:02:44 -0500

    persist user preference

--- hooks/useDarkMode.js
+++ hooks/useDarkMode.js
@@ -10,14 +10,23 @@ export default function useDarkMode() {
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
+      window.localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
+      window.localStorage.setItem("theme", "light");
     }
   }, [isDarkMode]);
 
   function initialThemeMode() {
-    if (typeof window !== "undefined" && window.matchMedia) {
-      return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (typeof window !== "undefined") {
+      const memory = localStorage.getItem("theme");
+      if (memory != null) {
+        return memory === "dark";
+      }
+
+      if (window.matchMedia != null) {
+        return window.matchMedia("(prefers-color-scheme: dark)").matches;
+      }
     }
 
     return false;
