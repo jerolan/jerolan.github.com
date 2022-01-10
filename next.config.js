a3de4b18020a81eed119c9e833dea40e2fb73065
Jerome Olvera <jerome.olvera@gmail.com>
2022-01-09 21:47:20 -0600

    use defaults for next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
 const withOptimizedImages = require("next-optimized-images");
 
-module.exports = withOptimizedImages({});
+module.exports = withOptimizedImages({
+  inlineImageLimit: 8192,
+  imagesFolder: "images",
+  imagesName: "[name]-[hash].[ext]",
+  handleImages: ["jpeg", "png", "svg", "webp", "gif"],
+  removeOriginalExtension: false,
+  optimizeImages: true,
+  optimizeImagesInDev: false,
+  mozjpeg: {
+    quality: 80,
+  },
+  optipng: {
+    optimizationLevel: 3,
+  },
+  pngquant: false,
+  gifsicle: {
+    interlaced: true,
+    optimizationLevel: 3,
+  },
+  svgo: {
+    // enable/disable svgo plugins here
+  },
+  webp: {
+    preset: "default",
+    quality: 75,
+  },
+});
